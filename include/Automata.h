4d7901f0511e8d5a2a02a2bb8ebdee3284bd4052
ceko98 <ceko9898@gmail.com>
2021-07-03 11:55:03 +0300

    Add automata class

---
+++ include/Automata.h
@@ -0,0 +1,22 @@
+#ifndef __AUTOMATA_H___
+#define __AUTOMATA_H___
+
+class Automata
+{
+private:
+
+public:
+    Automata(/* args */);
+    ~Automata();
+};
+
+Automata::Automata(/* args */)
+{
+}
+
+Automata::~Automata()
+{
+}
+
+
+#endif
